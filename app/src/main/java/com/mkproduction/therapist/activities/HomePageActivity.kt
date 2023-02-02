3c1432b17dcbd776e7985d3719bc2090b60b3ef0
MardinK522 <kamblemardin@gmail.com>
2023-02-02 19:36:19 +0530

    MedicineFragment.kt has been added

--- app/src/main/java/com/mkproduction/therapist/activities/HomePageActivity.kt
+++ app/src/main/java/com/mkproduction/therapist/activities/HomePageActivity.kt
@@ -7,14 +7,16 @@ import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mkproduction.therapist.R
 import com.mkproduction.therapist.adapters.FragmentViewPagerStateAdapter
 import com.mkproduction.therapist.fragments.HomeFragment
+import com.mkproduction.therapist.fragments.MedicineFragment
 import com.mkproduction.therapist.fragments.PaymentFragment
-import com.mkproduction.therapist.fragments.ProfileFragment
 
 class HomePageActivity : AppCompatActivity() {
+    private lateinit var mToolbar: MaterialToolbar
     private lateinit var mBottomNavigationBar: BottomNavigationView
     private lateinit var mFragmentViewPager2: ViewPager2
     private var doublePressed = false
@@ -42,18 +44,21 @@ class HomePageActivity : AppCompatActivity() {
         mFragmentViewPager2.isUserInputEnabled = false
         mFragmentViewPager2.adapter = FragmentViewPagerStateAdapter(
             this@HomePageActivity
-        ).addFragment(HomeFragment(), PaymentFragment(), ProfileFragment())
+        ).addFragment(HomeFragment(), PaymentFragment(), MedicineFragment())
         mBottomNavigationBar.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_bar_home_item -> mFragmentViewPager2.currentItem = 0
                 R.id.navigation_bar_payment_item -> mFragmentViewPager2.currentItem = 1
-                R.id.navigation_bar_profile_item -> mFragmentViewPager2.currentItem = 2
+                R.id.navigation_bar_profile_item -> {
+                    mFragmentViewPager2.currentItem = 2
+                }
             }
             true
         }
     }
 
     private fun findViews() {
+        mToolbar = findViewById(R.id.home_activity_tool_action_bar)
         mBottomNavigationBar = findViewById(R.id.home_activity_bottom_navigation_bar)
         mFragmentViewPager2 = findViewById(R.id.home_activity_fragment_view_pager2)
     }
--- app/src/main/java/com/mkproduction/therapist/fragments/MedicineFragment.kt
+++ app/src/main/java/com/mkproduction/therapist/fragments/MedicineFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mkproduction.therapist.R
 
-class ProfileFragment : Fragment() {
+class MedicineFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
